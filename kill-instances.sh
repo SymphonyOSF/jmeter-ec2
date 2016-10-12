90dd1743b7e58fd6b9a28d20a34b886f85fd84c0
Matt Arturi <matthew.arturi@.symphony.com>
2016-10-12 13:03:28 -0400

    Added a flag for removing instances

--- kill-instances.sh
+++ kill-instances.sh
@@ -2,9 +2,11 @@
 
 REGION="us-east-1"
 
-attempted_instanceids=`cat ${BUILD_NUMBER}_instanceIds.txt`
-aws ec2 terminate-instances --instance-ids ${attempted_instanceids[@]} \
-  --region $REGION \
-  --output text \
-  --query 'TerminatingInstances[].InstanceId'
-echo
+if [ $KILL_INSTANCES -eq "true" ] ; then
+	attempted_instanceids=`cat ${BUILD_NUMBER}_instanceIds.txt`
+	aws ec2 terminate-instances --instance-ids ${attempted_instanceids[@]} \
+	  --region $REGION \
+	  --output text \
+	  --query 'TerminatingInstances[].InstanceId'
+	echo
+fi
